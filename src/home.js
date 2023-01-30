cac85d6890cf302580826ae40f11d49b89f60b04
KenMR <kmr5591@gmail.com>
2023-01-29 19:45:49 -0500

    Add code to generate full homepage

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import plate from './images/plate.png';
 
 export default function createHome () {
   console.log('I am creating the home page!');
-  
+
   const content = document.getElementById('content');
   const body = document.createElement('div');
   const nav = document.createElement('div');
@@ -55,22 +55,84 @@ export default function createHome () {
   header.appendChild(h1);
   header.appendChild(icon2);
 
-  // create information section for the home page
   const info = document.createElement('div');
   info.setAttribute('id', 'info');
 
+  // description section
   const description = document.createElement('div');
-  const hours = document.createElement('div');
-  const location = document.createElement('div');
-
-  const descriptionText = document.createTextNode('Description');
-  const hoursText = document.createTextNode('Hours');
-  const locationText = document.createTextNode('Location');
+  description.classList.add('home-item');
+  const descriptionText =
+  document.createTextNode("Come to the best diner in the state! Our top notch staff will serve you a breakfast you'll never forget! We guarantee you'll want to come back again and again!");
+  let name = document.createElement('h2');
+  let nameText = document.createTextNode('Ken');
+  name.appendChild(nameText);
 
   description.appendChild(descriptionText);
-  hours.appendChild(hoursText);
+  description.appendChild(name);
+
+  // hours section
+  const hours = document.createElement('div');
+  hours.classList.add('home-item');
+
+  const hoursHeader = document.createElement('h2');
+  const hoursHeaderText = document.createTextNode('Hours');
+  hoursHeader.appendChild(hoursHeaderText);
+
+  const dateList = document.createElement('ul');
+
+  const sunday = document.createElement('li');
+  const sundayText = document.createTextNode('Sunday: 8am - 5pm');
+  sunday.appendChild(sundayText);
+
+  const monday = document.createElement('li');
+  const mondayText = document.createTextNode('Monday: Closed');
+  monday.appendChild(mondayText);
+
+  const tuesday = document.createElement('li');
+  const tuesdayText = document.createTextNode('Tuesday: 6am - 2pm');
+  tuesday.appendChild(tuesdayText);
+
+  const wednesday = document.createElement('li');
+  const wednesdayText = document.createTextNode('Wednesday: 6am - 2pm');
+  wednesday.appendChild(wednesdayText);
+
+  const thursday = document.createElement('li');
+  const thursdayText = document.createTextNode('Thursday: 6am - 2pm');
+  thursday.appendChild(thursdayText);
+
+  const friday = document.createElement('li');
+  const fridayText = document.createTextNode('Friday: 6am - 4pm');
+  friday.appendChild(fridayText);
+
+  const saturday = document.createElement('li');
+  const saturdayText = document.createTextNode('Saturday: 8am - 4pm');
+  saturday.appendChild(saturdayText);
+
+  dateList.appendChild(sunday);
+  dateList.appendChild(monday);
+  dateList.appendChild(tuesday);
+  dateList.appendChild(wednesday);
+  dateList.appendChild(thursday);
+  dateList.appendChild(friday);
+  dateList.appendChild(saturday);
+
+  hours.appendChild(hoursHeader);
+  hours.appendChild(dateList);
+
+  // location section
+  const location = document.createElement('div');
+  location.classList.add('home-item');
+
+  const locationHeader = document.createElement('h2');
+  const locationHeaderText = document.createTextNode('Location');
+  locationHeader.appendChild(locationHeaderText);
+
+  const locationText = document.createTextNode('123 Main Street, Towson, Maryland');
+
+  location.appendChild(locationHeader);
   location.appendChild(locationText);
 
+  // append all sections to finish page generation
   info.appendChild(description);
   info.appendChild(hours);
   info.appendChild(location);
